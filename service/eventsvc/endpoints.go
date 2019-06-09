cef71c0fa17b4bf50345ce920f8146cc38987ffd
Lucas Chen <chucobo5219@gmail.com>
2019-06-10 01:11:21 +0800

    Init Endpoint

---
+++ service/eventsvc/endpoints.go
@@ -0,0 +1,18 @@
+package eventsvc
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+func GetEventListEndpoint(db *gorm.DB, c *gin.Context) {
+}
+
+func PostEventListEndpoint(db *gorm.DB, c *gin.Context) {
+}
+
+func DeleteEventListEndpoint(db *gorm.DB, c *gin.Context) {
+}
+
+func UpdateEventListEndpoint(db *gorm.DB, c *gin.Context) {
+}
--- service/main.go
+++ service/main.go
@@ -1,29 +1,61 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/eventlist/service/eventsvc"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const (
+	DBHost   = "localhost"
+	Port     = "5432"
+	DBName   = "postgres"
+	Username = "postgres"
+	Password = "example"
+)
 
 func main() {
-	eventListPath := "/event-list"
+	// Setup Database
+	postgresConfig := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		DBHost, Port, DBName, Username, Password, "disable")
+	db, err := gorm.Open("postgres", postgresConfig)
+	if err != nil {
+		panic(fmt.Sprintf("Can not create db client: %v", err))
+	}
+	defer db.Close()
+
+	// Setup Gin Handler
+	eventListPath := "/event-list/"
 	r := gin.Default()
+
+	// Init Handler
 	r.GET(eventListPath, func(c *gin.Context) {
+		eventsvc.GetEventListEndpoint(db, c)
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.DELETE(eventListPath, func(c *gin.Context) {
+		eventsvc.DeleteEventListEndpoint(db, c)
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.PATCH(eventListPath, func(c *gin.Context) {
+		eventsvc.UpdateEventListEndpoint(db, c)
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.POST(eventListPath, func(c *gin.Context) {
+		eventsvc.PostEventListEndpoint(db, c)
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
